9e2b8e6a6a51a82bdc8db30457343aa19e712de5
Sven Weintuch <sven@weintuch.net>
2015-06-06 18:37:49 +0200

    added comments

--- goophry.php
+++ goophry.php
@@ -4,9 +4,27 @@ namespace Goophry;
 
 class Taskqueue
 {
+
+    /**
+    * The parameters for this Taskqueue instance.
+    *
+    * @var array
+    */
     protected $params;
+
+    /**
+    * The phpredis-instance.
+    *
+    * @var Redis
+    */
     protected $redis = null;
 
+    /**
+    * Creates a new Taskqueue instance.
+    *
+    * @param array $params
+    * @return void
+    */
     public function __construct(array $params = array())
     {
         if (!empty($params)) {
@@ -14,6 +32,12 @@ class Taskqueue
         }
     }
 
+    /**
+    * Reads a Goophry configuration file and merges it with the internal params.
+    *
+    * @param string $file
+    * @return bool
+    */
     public function readConfig($file)
     {
         if (!is_string($file) || !file_exists($file)) {
@@ -29,6 +53,12 @@ class Taskqueue
         return true;
     }
 
+    /**
+    * Merges the internal params with provided params.
+    *
+    * @param array $params
+    * @return void
+    */
     private function mergeParams(array $params)
     {
         $defaults = array(
@@ -52,6 +82,11 @@ class Taskqueue
         $this->params = array_merge($defaults, $params);
     }
 
+    /**
+    * Creates a phpredis instance and establishes connection to a Redis-server.
+    *
+    * @return bool
+    */
     protected function connect()
     {
         if (false === $this->redis) {
@@ -81,9 +116,18 @@ class Taskqueue
         return true;
     }
 
+    /**
+    * Adds a task.
+    *
+    * @param string|Task $task
+    * @param mixed $args,...
+    * @return bool
+    */
     public function addTask($task)
     {
         if (is_string($task)) {
+            // When the first argument was a string, it must be the task-type.
+            // So we create a Task-instance and set the passed arguments for it.
             $type = $task;
             $task = new Task();
             $task->setType($type);
@@ -96,6 +140,12 @@ class Taskqueue
         return $this->addTaskObj($task);
     }
 
+    /**
+    * Pushes a task entry to Redis.
+    *
+    * @param Task $task
+    * @return bool
+    */
     protected function addTaskObj(Task $task)
     {
         if (false === $this->connect()) {
@@ -111,6 +161,12 @@ class Taskqueue
         return true;
     }
 
+    /**
+    * Pops a task entry from a failed-task list, and returns it.
+    *
+    * @param string $type
+    * @return bool|Task
+    */
     public function popFailedTask($type)
     {
         if (false === $this->connect()) {
@@ -132,17 +188,42 @@ class Taskqueue
         return $task;
     }
 
-    public function getFailedTask($type, $index)
+    /**
+    * Alias for popFailedTask, for backwards compatibility.
+    * Deprecated - do not use any more.
+    *
+    * @param string $type
+    * @return bool|Task
+    */
+    public function getFailedTask($type)
     {
         return $this->popFailedTask($type);
     }
 }
 
+
 class Task
 {
-    protected $type = false;
+
+    /**
+    * The Task-type of this instance.
+    *
+    * @var string
+    */
+    protected $type = '';
+
+    /**
+    * The Task properties of this instance.
+    *
+    * @var array
+    */
     protected $task;
 
+    /**
+    * Creates a new Task instance.
+    *
+    * @return void
+    */
     public function __construct()
     {
         $this->task = array(
@@ -151,31 +232,65 @@ class Task
         );
     }
 
+    /**
+    * Set the type of this task.
+    *
+    * @param string $type
+    * @return void
+    */
     public function setType($type)
     {
         $this->type = $type;
     }
 
+    /**
+    * Get the type of this task.
+    *
+    * @return string
+    */
     public function getType()
     {
         return $this->type;
     }
 
+    /**
+    * Set the arguments for this task.
+    *
+    * @param array $args
+    * @return void
+    */
     public function setArgs(array $args)
     {
         $this->task['Args'] = $this->encodeArgs($args);
     }
 
+    /**
+    * Get the arguments for this task.
+    *
+    * @return array
+    */
     public function getArgs()
     {
         return $this->task['Args'];
     }
 
+    /**
+    * Adds an argument for this task.
+    *
+    * @param mixed $arg
+    * @return void
+    */
     public function addArg($arg)
     {
         $this->task['Args'][] = $this->encodeArg($arg);
     }
 
+    /**
+    * Get the argument-value for at the given index.
+    *
+    * @param int $index
+    * @return bool|string
+    */
     public function getArg($index = 0)
     {
         if (!isset($this->task['Args'][$index])) {
@@ -184,11 +299,21 @@ class Task
         return $this->task['Args'][$index];
     }
 
+    /**
+    * Get the ErrorMessage for this task.
+    *
+    * @return string
+    */
     public function getErrorMessage()
     {
         return $this->task['ErrorMessage'];
     }
 
+    /**
+    * Get this json-encoded value for this task, as it is pushed to Redis.
+    *
+    * @return string
+    */
     public function getJson()
     {
         $task = $this->task;
@@ -198,6 +323,12 @@ class Task
         return json_encode($task);
     }
 
+    /**
+    * Sets the properties for this task, from the json-encoded value given from Redis.
+    *
+    * @param string $string
+    * @return bool
+    */
     public function parseJson($string)
     {
         $task = json_decode($string, true);
@@ -208,6 +339,12 @@ class Task
         return true;
     }
 
+    /**
+    * Encodes the given array of arguments.
+    *
+    * @param array $args
+    * @return array
+    */
     protected function encodeArgs(array $args)
     {
         foreach ($args as &$arg) {
@@ -216,6 +353,12 @@ class Task
         return $args;
     }
 
+    /**
+    * Encodes a single argument.
+    *
+    * @param mixed $arg
+    * @return string
+    */
     protected function encodeArg($arg)
     {
         if (is_object($arg) || is_array($arg)) {
