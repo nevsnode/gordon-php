c9824ec45bb1711b770682059d243c9dcef40f3b
Sven Weintuch <sven@weintuch.net>
2015-06-07 13:18:24 +0200

    moved task/taskqueue classes into src directory

---
+++ src/Task.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace Goophry;
+
+class Task
+{
+
+    /**
+    * The Task-type of this instance.
+    *
+    * @var string
+    */
+    protected $type = '';
+
+    /**
+    * The Task properties of this instance.
+    *
+    * @var array
+    */
+    protected $task;
+
+    /**
+    * Creates a new Task instance.
+    *
+    * @return void
+    */
+    public function __construct()
+    {
+        $this->task = array(
+            'Args'          => array(),
+            'ErrorMessage'  => '',
+        );
+    }
+
+    /**
+    * Set the type of this task.
+    *
+    * @param string $type
+    * @return void
+    */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+    * Get the type of this task.
+    *
+    * @return string
+    */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+    * Set the arguments for this task.
+    *
+    * @param array $args
+    * @return void
+    */
+    public function setArgs(array $args)
+    {
+        $this->task['Args'] = $this->encodeArgs($args);
+    }
+
+    /**
+    * Get the arguments for this task.
+    *
+    * @return array
+    */
+    public function getArgs()
+    {
+        return $this->task['Args'];
+    }
+
+    /**
+    * Adds an argument for this task.
+    *
+    * @param mixed $arg
+    * @return void
+    */
+    public function addArg($arg)
+    {
+        $this->task['Args'][] = $this->encodeArg($arg);
+    }
+
+    /**
+    * Get the argument-value for at the given index.
+    *
+    * @param int $index
+    * @return bool|string
+    */
+    public function getArg($index = 0)
+    {
+        if (!isset($this->task['Args'][$index])) {
+            return false;
+        }
+        return $this->task['Args'][$index];
+    }
+
+    /**
+    * Get the ErrorMessage for this task.
+    *
+    * @return string
+    */
+    public function getErrorMessage()
+    {
+        return $this->task['ErrorMessage'];
+    }
+
+    /**
+    * Get this json-encoded value for this task, as it is pushed to Redis.
+    *
+    * @return string
+    */
+    public function getJson()
+    {
+        $task = $this->task;
+        if (empty($task['ErrorMessage'])) {
+            unset($task['ErrorMessage']);
+        }
+        return json_encode($task);
+    }
+
+    /**
+    * Sets the properties for this task, from the json-encoded value given from Redis.
+    *
+    * @param string $string
+    * @return bool
+    */
+    public function parseJson($string)
+    {
+        $task = json_decode($string, true);
+        if (empty($task)) {
+            return false;
+        }
+        $this->task = array_merge($this->task, $task);
+        return true;
+    }
+
+    /**
+    * Encodes the given array of arguments.
+    *
+    * @param array $args
+    * @return array
+    */
+    protected function encodeArgs(array $args)
+    {
+        foreach ($args as &$arg) {
+            $arg = $this->encodeArg($arg);
+        }
+        return $args;
+    }
+
+    /**
+    * Encodes a single argument.
+    *
+    * @param mixed $arg
+    * @return string
+    */
+    protected function encodeArg($arg)
+    {
+        if (is_object($arg) || is_array($arg)) {
+            return base64_encode(json_encode($arg));
+        } else {
+            return strval($arg);
+        }
+    }
+}
